dbbd049ce69765da7ab84c84496fe3e16fdf6911
adhikarisanjib <adhikarisanjib1998@gmail.com>
2026-09-07 11:50:59 +0100

    Modify: For Production Update: docker database image to point to 127.0.0.1 Update: run the server on port 8003

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ def health_check():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=settings.DEBUG)
+    uvicorn.run("main:app", host="127.0.0.1", port=8003, reload=settings.DEBUG)
